eb034fc49892e8d5f9c50ab27dc47b63e5bb5067
BDesBrisay <bryce@desbrisay.com>
2017-03-16 11:54:43 -0700

    Updated Lab 7

--- lab7/BryceDesbrisayLab7.py
+++ lab7/BryceDesbrisayLab7.py
@@ -6,8 +6,8 @@ def main():
     while i < 5:
         results = []
         fileName = input("Please enter the file name: ")
-        with open(fileName, encoding='utf-8') as file:
-            for line in file:
+        with open(fileName, encoding='utf-8') as list:
+            for line in list:
                 line = line.rstrip()
                 results.append(line)
             if all(line.isdigit() for line in results):
@@ -20,3 +20,17 @@ def main():
         i += 1
 main()
 
+'''
+Test cases:
+
+Please enter the file name: bad1.dat
+Error: file contents invalid
+Please enter the file name: bad2.dat
+Error: file contents invalid
+Please enter the file name: bad3.dat
+Error: file contents invalid
+Please enter the file name: bad4.dat
+Error: file contents invalid
+Please enter the file name: good.dat
+The sum is 55
+'''
